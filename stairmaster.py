a55e355203998fe626df96618cb64f69564f55e1
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-05-23 11:42:49 -0700

    implements dynamic programming solution

--- stairmaster.py
+++ stairmaster.py
@@ -20,10 +20,21 @@
 # 1. 1 step + 1 step + 1 step
 # 2. 1 step + 2 steps
 # 3. 2 steps + 1 step
- 
-def climb_stairs(n):
-    pass
 
+def climb_stairs(n, memo=None):
+    if n == 1 or n == 2:
+        return n
+
+    memo = memo if memo is not None else {}
+
+    if n in memo:
+        return memo[n]
+    
+    result = (climb_stairs(n-1, memo) + climb_stairs(n-2, memo))
+
+    memo[n] = result
+
+    return result
     # at each step, how many choices do we have?
     # given each choice, how many combinations of steps are left?
     # is there a threshold below which we have only a limited number of combinations?
